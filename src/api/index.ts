30837f32f2c1df2d5027467da8a2339591201b5f
Alexey <aezz120813@gmail.com>
2022-05-16 19:57:11 +0600

    refactor: move getting forecast weather to const from return

--- src/api/index.ts
+++ src/api/index.ts
@@ -43,6 +43,19 @@ export async function getWeather(city: string): Promise<WeeklyWeather> {
 
   const openweatherUrl = `${baseOpenweatherApiUrl}/data/2.5/onecall?exclude=minutely,hourly,alerts&lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_OPENWEAHER_API_KEY}&units=metric&lang=ru`;
   const weather = (await fetch(openweatherUrl).then((res) => res.json())) as FetchedWeather;
+  const forecastWeather = nextFiveDaysFilter(
+    weather.daily.map((weather) => {
+      weather.dt = weather.dt * 1000; // приходит время 1970 года, если добавить три нуля, то получится актуальное
+      return weather;
+    }),
+    'dt',
+  ).map((weather: FetchedWeather['daily'][0]) => ({
+    date: new Date(weather.dt),
+    feelsLike: weather.feels_like.day,
+    humidity: weather.humidity,
+    pressure: weather.pressure,
+    temp: weather.temp.day,
+  }));
 
   return {
     currentWeather: {
@@ -51,18 +64,6 @@ export async function getWeather(city: string): Promise<WeeklyWeather> {
       humidity: weather.current.humidity,
       pressure: weather.current.pressure,
     },
-    forecastWeather: nextFiveDaysFilter(
-      weather.daily.map((weather) => {
-        weather.dt = weather.dt * 1000; // приходит время 1970 года, если добавить три нуля, то получится актуальное
-        return weather;
-      }),
-      'dt',
-    ).map((weather: FetchedWeather['daily'][0]) => ({
-      date: new Date(weather.dt),
-      feelsLike: weather.feels_like.day,
-      humidity: weather.humidity,
-      pressure: weather.pressure,
-      temp: weather.temp.day,
-    })),
+    forecastWeather,
   } as WeeklyWeather;
 }
